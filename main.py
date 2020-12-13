1678e82de586b14956dc68592557373b6139e1be
NicoCaz <nicolascazorlamartinez@gmail.com>
2020-12-13 14:48:15 -0300

    Agrego Mayusculas al cesar

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-letras=("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-
-palabra="el rio esta frio y con un pez"
-num=3
-#print(letras.index("a"))
-
+letras=("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ")
+palabra="El rio esta frio y con un pez"
 def incriptaCesar(palabra,num):
     i=0
     palabra.lower()
     resultado=""
     while i< len(palabra):
-        try:
+        try: #Letras minusculas y espacios
             indice=letras.index(palabra[i])
-            if indice+num >len(letras):
-                indice=indice-len(letras)
-            resultado= resultado + letras[indice+num]
-        except:
-            resultado =resultado + " "
+            if indice==len(letras)-1:
+                resultado =resultado + " "
+            else:    
+                if indice+num >len(letras)-1:
+                    indice=indice-len(letras)-1
+                resultado= resultado + letras[indice+num]
+        except:#Mayusculas
+            indice=letras.index(palabra[i].lower())
+            if indice+num >len(letras)-1:
+                    indice=indice-len(letras)-1
+            resultado= resultado + letras[indice+num].upper()
+        
         i=i+1
     
     print(resultado)
    
-
 incriptaCesar(palabra,20)
