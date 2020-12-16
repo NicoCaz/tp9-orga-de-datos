3fd7a073230adfa1eda94ce8783f813c5d17402f
NicoCaz <nicolascazorlamartinez@gmail.com>
2020-12-16 13:24:13 -0300

    incriptacion ensalada

--- main.py
+++ main.py
@@ -1,71 +1,77 @@
 
-letras=("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ")
+letras=("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 palabra="el rio esta frio y con un pez"
 
 
-def control(indice):
-    if indice >len(letras)-1:
-        return indice-len(letras)-1
+def control(indice,largo):
+    if indice >len(largo)-1:
+        return indice-len(largo)-1
     else:
         return indice
 
 
-
-
 def incriptaCesar(palabra,num):
     i=0
-    palabra.lower()
     resultado=""
     while i< len(palabra):
-        
-        try: #Letras minusculas y espacios
-            indice=letras.index(palabra[i])
-            if indice==len(letras)-1:
-                resultado =resultado + " "
-            else:    
-                indice= control(indice)
-                resultado= resultado + letras[indice+num]
-        except:#Mayusculas
-            indice=letras.index(palabra[i].lower())
-            if indice+num >len(letras)-1:
+        if palabra[i]==" ":
+            resultado =resultado + " "
+        else:    
+            try: #Letras minusculas y espacios
+                indice=letras.index(palabra[i])
+                indice= control(indice+num,letras)
+                resultado= resultado + letras[indice]
+            except:#Mayusculas
+                indice=letras.index(palabra[i].lower())
+                if indice+num >len(letras)-1:
                     indice=indice-len(letras)-1
-            resultado= resultado + letras[indice+num].upper()
-            letras[indice+num].lower()
+                resultado= resultado + letras[indice+num].upper()
+                letras[indice+num].lower()
         
-        i=i+1
+    i=i+1
     
     print(resultado)
    
-incriptaCesar(palabra,20)
+#incriptaCesar(palabra,20)
 
 
-def incriptaCesarModificado(palabra,num):
-    
+clave="secreto"
 
+def incriptacionvigenere(palabra,clave):
+    resultado=""
     i=0
-    cant=0
-    while i<len(palabra):
-        letrasMod[num+i]=palabra[i]
+    j=0
+    indice=0
+    caracter=""
+    while i< len(palabra):
+        if palabra[i]==" ":
+            resultado=resultado+" "
+            caracter=" "
+        else:
+            indice=letras.index(palabra[i])       
+            try:  
+                indice = control((indice+letras.index(clave[j])),letras)
+                caracter=letras[indice]
+                resultado = resultado + caracter
+            except:
+                indice=letras.index(palabra[i].lower())
+                indice= control((indice+letras.index(clave[j])),letras)
+                caracter= letras[indice].upper()
+                resultado= resultado + caracter
+                letras[indice].lower()        
         i=i+1
-    indice=len(palabra)+num
-    i=0
-    while cant<len(letras):
-        indice=control(indice)
-        caracter=letras[i]
-        try:
-            palabra[caracter]           
-        except:
-            letrasMod[indice:caracter]
-
-        i=i+1
-        cant=cant+1
-
-    print(letrasMod)   
-        
-
-#incriptaCesarModificado("murcielago",5)
+        if j>len(clave)-2:
+            j=0
+        else:
+            if indice<len(letras)-2 and caracter!=" ":
+                j=j+1
+            
+    print(resultado)
 
 
+incriptacionvigenere(palabra,clave)
 
-def incriptacionvigenere(palabra):
-    pass
+
+#letras.index(clave[control(j,clave)])
+
+#wp tzs xgle himh m usp lr isr
